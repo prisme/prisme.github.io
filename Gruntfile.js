37e8caf2fcf3017bedb4d71f70aafd37942935cb
prisme <jeremy.saintprix@gmail.com>
2013-09-07 18:22:18 +0200

    grunt : stop running jsLint on /scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function (grunt) {
             },
             all: [
                 'Gruntfile.js',
-                '<%= yeoman.app %>/scripts/{,*/}*.js',
+                // '<%= yeoman.app %>/scripts/{,*/}*.js',
                 '!<%= yeoman.app %>/scripts/vendor/*',
                 'test/spec/{,*/}*.js'
             ]
