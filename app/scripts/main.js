13faa0901bc6922b86729e92906e075659da75ad
prisme <jeremy.saintprix@gmail.com>
2013-09-07 18:46:07 +0200

    Js : main file, require config

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,54 +1,18 @@
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
-        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
-        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
-        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
-        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
-        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
-        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
-        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
-        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
-        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
-        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition'
+        three: '../bower_components/threejs/build/three.min',
     },
     shim: {
-        bootstrapAffix: {
-            deps: ['jquery']
-        },
-        bootstrapAlert: {
-            deps: ['jquery']
-        },
-        bootstrapButton: {
-            deps: ['jquery']
-        },
-        bootstrapCarousel: {
-            deps: ['jquery']
-        },
-        bootstrapCollapse: {
-            deps: ['jquery']
-        },
-        bootstrapPopover: {
-            deps: ['jquery']
-        },
-        bootstrapScrollspy: {
-            deps: ['jquery']
-        },
-        bootstrapTab: {
-            deps: ['jquery']
-        },
-        bootstrapTooltip: {
-            deps: ['jquery']
-        },
-        bootstrapTransition: {
-            deps: ['jquery']
+        'three': {
+            exports: 'THREE'
         }
-    }
+    },
 });
 
-require(['app', 'jquery'], function (app, $) {
+require(['scene'], function() {
     'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+
+    var vcard = document.querySelector('.jumbotron')
+    vcard.className += ' active'
 });
